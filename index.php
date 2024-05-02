6e40086d2ca4b723fad3a02976661179a21aa07f
YOHAN ALEK PLAZOLA ARANGURE <2021030101@upsin.edu.mx>
2024-05-01 18:22:38 -0700

    Comentarios

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
 echo 'Hola mundo';
+
+?>
+
+<?php 
+
+echo 'Esto se concatenara en la salida'
+
+?>
---
+++ sintaxis/comentarios.php
@@ -0,0 +1,12 @@
+<?php
+
+// Comentario de 1 línea
+
+# Comentario de 1 línea
+
+/* 
+   Comentario
+   de
+   multiples
+   líneas
+*/
